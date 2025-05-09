d1fd920185c1a1f13c43a831582521de23add303
Franfc44 <mfran3921@gmail.com>
2025-05-09 15:24:10 -0300

    sonido
    sonido funcionando

---
+++ ImmortalGame/Assets/Scripts/AudioVolumen.cs
@@ -0,0 +1,37 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+public class AudioVolumen : MonoBehaviour
+{
+
+
+
+
+    public Slider controlVolumen;
+
+    public GameObject[] audios;
+
+
+    private void Start()
+    {
+        audios = GameObject.FindGameObjectsWithTag("audio");
+
+        controlVolumen.value = PlayerPrefs.GetFloat("volumenSave", 1f);
+      
+
+    }
+    private void Update()
+    {
+        foreach (GameObject au in audios)
+            au.GetComponent<AudioSource>().volume = controlVolumen.value;
+    }
+    public void GuardarVolumen ()
+    {
+        PlayerPrefs.SetFloat("VolumenSave", controlVolumen.value);
+    }
+
+    
+
+
+
+}
